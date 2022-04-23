3efe1133f45214a62f3b38ec6631c98a2be07716
felipemaxrosa <felipemax.suporteti@gmail.com>
2022-04-22 21:16:17 -0300

    25. Generating HTML files html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // 4 Types of Asset Modules
 // - asset/resource - to large files,
@@ -15,7 +16,7 @@ module.exports = {
 	output: {
 		filename: 'bundle.[contenthash].js',
 		path: path.resolve(__dirname, './dist'),
-		publicPath: 'dist/',
+		publicPath: '',
 	},
 	mode: 'none',
 	module: {
@@ -54,5 +55,6 @@ module.exports = {
 				path.join(process.cwd(), 'build/**/*'),
 			],
 		}),
+		new HtmlWebpackPlugin(),
 	],
 };
